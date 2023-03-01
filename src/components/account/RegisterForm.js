3611f7147382f06e27bfd464fa41eca099d472a5
xd <kk>
2023-03-01 14:37:32 -0600

    register

--- src/components/account/RegisterForm.js
+++ src/components/account/RegisterForm.js
@@ -1,25 +1,39 @@
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Button} from 'react-native'
 import React from "react";
-import {Input} from "react-native-elements";
+import {Icon, Input} from "react-native-elements";
 
-export default function RegisterForm(){
+export default function RegisterForm() {
 
-    return(
-        <View>
-          <Input/>
+    return (
+        <View style={styles.viewForm}>
+            <Input
+                containerStyle={styles.input} placeholder='Correo electronico'
+                rightIcon={<Icon type="material-community" name="at" iconStyle={styles.icon}/>}
+            />
+            <Input
+                containerStyle={styles.input} placeholder='Contraseña'
+                rightIcon={<Icon type="material-community" name="eye-outline" iconStyle={styles.icon}/>}
+            />
+            <Input
+                containerStyle={styles.input} placeholder='Repetir contraseña'
+                rightIcon={<Icon type="material-community" name="eye-outline" iconStyle={styles.icon}/>}
+            />
+            <Button title={"Registrarse"}/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    logo:{
-        width:"100%",
-        height:150,
-        resizeMode:"contain",
-        marginTop:30
-    },
-    form:{
-        marginHorizontal:40,
+    viewForm: {
+        marginTop: 30,
 
+    },
+    input: {
+        width: "100%",
+        marginTop: 20,
+
+    },
+    icon: {
+        color: "#c1c1c1"
     }
 })
